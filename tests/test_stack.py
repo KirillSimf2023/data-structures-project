704be94395ccd11a28fd0b69a657a984c7c01801
kirill <lobbi.kirill@mail.ru>
2023-05-24 19:33:13 +0300

    Напиcаны тесты в tests/test_stack.py для класса Node.

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,1 +1,14 @@
 """Здесь надо написать тесты с использованием unittest для модуля stack."""
+import unittest
+from src.stack import Node, Stack
+
+class TestNode(unittest.TestCase):
+    def test_init(self):
+        data_1 = Node(5, None)
+        self.assertEqual(data_1.data, 5)
+        self.assertEqual(data_1.next_node, None)
+
+        data_2 = Node('a', data_1)
+        self.assertEqual(data_2.data, 'a')
+        self.assertEqual(data_2.next_node.data, 5)
+
