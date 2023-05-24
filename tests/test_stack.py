8d06cbd7ec3a7c04be96ec2d038debb9e5a8c6c1
kirill <lobbi.kirill@mail.ru>
2023-05-24 19:33:48 +0300

    Напиcаны тесты в tests/test_stack.py для класса Stack.

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -12,3 +12,21 @@ class TestNode(unittest.TestCase):
         self.assertEqual(data_2.data, 'a')
         self.assertEqual(data_2.next_node.data, 5)
 
+
+
+
+class TestStack(unittest.TestCase):
+    def test_init(self):
+        stack = Stack()
+        self.assertEqual(stack.my_stack, [])
+
+    def test_push(self):
+        stack = Stack()
+        stack.push(5)
+        self.assertEqual(stack.my_stack[-1].data, 5)
+        self.assertEqual(stack.my_stack[-1].next_node, None)
+        stack.push("a")
+        self.assertEqual(len(stack.my_stack), 2)
+        self.assertEqual(stack.my_stack[-1].data, "a")
+        self.assertEqual(stack.my_stack[-1].next_node, stack.my_stack[0])
+
