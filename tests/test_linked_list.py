91412fe4ba86e671ce2649f23711a338210357db
kirill <lobbi.kirill@mail.ru>
2023-06-06 23:34:28 +0300

    Добавлены тесты для класса Node tests/test_linked_list.py

--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -1,1 +1,13 @@
 """Здесь надо написать тесты с использованием unittest для модуля linked_list."""
+import unittest
+from src.linked_list import Node, LinkedList
+
+class TestNode(unittest.TestCase):
+    def test_init(self):
+        data_1 = Node(5, None)
+        self.assertEqual(data_1.data, 5)
+        self.assertEqual(data_1.next_node, None)
+
+        data_2 = Node(10, data_1)
+        self.assertEqual(data_2.data, 10)
+        self.assertEqual(data_2.next_node.data, 5)
